42c6689e658d535e0a8695c76f5bf0c5717fa36a
Bayzidbaky1 <ethensaami4@gmail.com>
2023-01-04 19:49:34 +0600

    first commit

---
+++ lib/data.dart
@@ -0,0 +1,31 @@
+List<String> images = [
+  "https://www.freepnglogos.com/uploads/google-play-png-logo/google-play-png-logo-1.png",
+  "https://www.freepnglogos.com/uploads/youtube-logo-15.png",
+  "https://www.freepnglogos.com/uploads/netflix-logo-0.png",
+  "https://www.freepnglogos.com/uploads/hotstar-logo-png/hotstar-dallas-yoga-fest-best-yoga-festival-dallas-fitness-7.png",
+  "https://www.freepnglogos.com/uploads/disney-channel-png-logo/disney-channel-logo-png-symbol-2.png",
+  "https://www.freepnglogos.com/uploads/amazon-music-logo-png-14.png",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Hoichoi-720p-transperant.png/800px-Hoichoi-720p-transperant.png?20190420063217",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Chorki_Logo.png/220px-Chorki_Logo.png",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Binge_logo.png/800px-Binge_logo.png?20200528065959",
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Bioscope_Logo_Art_2018.svg/400px-Bioscope_Logo_Art_2018.svg.png",
+  "https://www.mcphagwara.com/wp-content/uploads/2022/07/Movies-Available-in-SouthFreak.jpg",
+  "https://apklust.com/logos/mlwbd-apk.png",
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTD08WtoSlKvPEtn34M5Ol11aZVUKwLiYb7VA&usqp=CAU",
+];
+
+List<String> links = [
+  "https://play.google.com/store/apps/details?id=com.google.android.youtube",
+  "https://www.youtube.com/",
+  "https://www.netflix.com/",
+  "https://www.hotstar.com/",
+  "https://www.disney.com/",
+  "https://www.amazon.com/",
+  "https://www.hoichoi.tv/",
+  "https://www.chorki.com/",
+  "https://binge.buzz/login",
+  "https://www.bioscopelive.com/en/",
+  "https://southfreak.pics/",
+  "https://mlwbd.city/movie/",
+  "https://reqzone.com/browse?type=movies",
+];
